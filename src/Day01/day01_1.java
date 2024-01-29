aecb268ffef31bd7bc34152dc567dee90cbf697b
Umin73 <trillionumin@gmail.com>
2024-01-29 20:15:12 +0900

    240129

---
+++ src/Day01/day01_1.java
@@ -0,0 +1,12 @@
+package Day01;
+
+public class day01_1 {
+    public static void main(String[] args) {
+        System.out.println(solution(2, 3));
+        System.out.println(solution(100, 2));
+    }
+
+    static int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ src/Day01/day01_3.java
@@ -0,0 +1,12 @@
+package Day01;
+
+public class day01_3 {
+    public static void main(String[] args) {
+        System.out.println(solution(3, 4));
+        System.out.println(solution(27, 19));
+    }
+
+    static int solution(int num1, int num2) {
+        return num1*num2;
+    }
+}
